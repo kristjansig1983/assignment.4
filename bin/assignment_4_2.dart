a7d8f03c4c76074f2d2b65f29b48d1441659254b
kristjansig1983 <91916422+kristjansig1983@users.noreply.github.com>
2022-03-15 17:50:19 +0000

    Vesen
    Er í bölvuðu veseni

--- bin/assignment_4_2.dart
+++ bin/assignment_4_2.dart
@@ -7,11 +7,10 @@ void main() {
 
   int item = 0;
   List<int> calculate_sum = [13,56,77,3,45,6,944,1,43,7,40,55,67,93,45,754,33];
-  for(item in calculate_sum){
-    if((item >= 20) && (item <= 80)){
+  for(int item in calculate_sum){
+    if(item >= 20 && item <= 80){
       print(item);
     }
   }
   print(item);
-
 }
---
+++ bin/assignment_4_3.dart
@@ -0,0 +1,16 @@
+import 'dart:math';
+import 'dart:convert';
+import 'dart:io';
+
+void main() {
+
+  for(int i = 0; i < 10; i++) {
+    if(true) {
+      print('Enter a number:');
+      int i = int.tryParse(stdin.readLineSync());
+    }
+  }
+  }
+
+
+
